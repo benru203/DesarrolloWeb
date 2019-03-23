b288ec4aa8e7e448451c21c473ae72c4dd9122a3
PABON PALACIO RUBEN <rpabon2@cuc.edu.co>
2019-03-23 16:09:51 -0500

    mejorado

---
+++ js/main.js
@@ -0,0 +1,37 @@
+window.onload = ()=>{
+    let button = document.querySelector('button');
+    button.addEventListener('click',(e)=>{
+        e.defaultPrevented;
+        let form = document.querySelector('form');
+        let inputs = form.querySelectorAll('input');
+        for (let index = 0; index < inputs.length; index++) {
+            console.log(validator(inputs[index]));
+            if(validator(inputs[index],'required|min:10')){            
+                inputs[index].classList.add("errorClass");
+                break;
+            }       
+        }
+        console.log('enviar datos');
+       // validator('algo','required|numeric');        
+    });
+};
+
+function validator(element, validator){
+    let arrayValidator = validator.split("|");
+    for (let index = 0; index < arrayValidator.length; index++) {
+        if(arrayValidator[index] == 'required'){
+            return (element.value.length == 0)?false:true;
+        }
+        if(arrayValidator[index].indexOf("min") != -1){
+            let pos = arrayValidator[index].indexOf(":");
+            let num = parseInt(arrayValidator[index].substring(pos+1));
+            return (element.value.length == num)?false:true;        
+        }
+        if(arrayValidator[index].indexOf("max") != -1){
+            let pos = arrayValidator[index].indexOf(":");
+            let num = parseInt(arrayValidator[index].substring(pos+1));
+            return (element.value.length == num)?false:true;        
+        }
+    }
+    return true;
+}
